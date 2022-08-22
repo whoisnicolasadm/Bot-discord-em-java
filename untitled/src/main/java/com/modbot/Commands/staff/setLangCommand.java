7e5bcf462194db626bb2bc7076160bbb2dde4548
WhoIsNicolasAdm <73183149+whoisnicolasadm@users.noreply.github.com>
2022-08-22 20:51:56 -0300

    Adding some Moderation commands, only files, after I will add the code...

---
+++ untitled/src/main/java/com/modbot/Commands/staff/setLangCommand.java
@@ -0,0 +1,15 @@
+package com.modbot.Commands.staff;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class setLangCommand extends ListenerAdapter {
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        var invoke = event.getName();
+        if(invoke.equals("")){
+
+        }
+    }
+}
